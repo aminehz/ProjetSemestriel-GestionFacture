f3066d0711b2e8754491d9bcd0ac8cc4f1a587b3
aminehz <amine.hz.hz.98@gmail.com>
2023-03-31 17:03:41 +0100

    stock delete , add functions completed

--- ClientSide/src/components/Stock.js
+++ ClientSide/src/components/Stock.js
@@ -1,18 +1,18 @@
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
 const Stock= () =>{
 const [stock,setStock]=useState([]);
 const [productName, setProductName] = useState('');
 const [productQuantity, setProductQuantity] = useState('');
 const [productPrice, setProductPrice] = useState('');
-const [showDialog, setShowDialog] = useState(false);
+const [show,setShow]=useState(false);
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
 
-const handleShowDialog = () => {
-    setShowDialog(true);
-  };
-  const handleCloseDialog = () => {
-    setShowDialog(false);
-  };
+
 useEffect(()=>{
 axios.get('http://localhost:3000/getProducts')
 .then(response=>{
@@ -36,15 +36,25 @@ const deleteProduct = (productId,e) =>{
 
 const addProduct=(e)=>{
     e.preventDefault();
+    setShow(false);
 const newProduct={productName:productName,productPrice:productPrice,productQuantity:productQuantity};
-axios.post('http://localhost:3000/addProduct',newProduct)
-.then(console.log(newProduct));
+    axios.post('http://localhost:3000/addProduct',newProduct)
+    .then((response)=> console.log(response.data))
+    .then(window.alert('Product Added'))
+    .catch(error =>{
+        if(error.response.status === 500) {
+            window.alert('the name should be unique');
+        }
+    });
+
+
+
 
 }
 
 return (
     <div className="stock">
-        <button type='button' className='btn btn-secondary' id='addProductBtn' onClick={handleShowDialog}><div className='w-100'>Add Product</div></button>
+        <button type='button' className='btn btn-secondary' onClick={handleShow}  id='addProductBtn' ><div className='w-100'>Add Product</div></button>
         <table className=" container mt-3 table table-bordered border-dark" id="stockTable">
             <thead className="table-secondary">
                 <tr>
@@ -75,26 +85,39 @@ return (
             </tbody>
         </table>
 
+        <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Product</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <form onSubmit={addProduct}>
+       <div className='form-group'>
+           <label>Product Name</label>
+           <input type="text" className='form-control' id="productName" placeholder='Product Name' value={productName} onChange={(e) => setProductName(e.target.value)} />
+       </div>
+       <div className='form-group'>
+           <label>Product Price</label>
+           <input type="text" className='form-control' id="productPrice" placeholder='Product Price' value={productPrice} onChange={(e) => setProductPrice(e.target.value)} />
+       </div>
+       <div className='form-group'>
+           <label>Product Quantity</label>
+           <input type="text" className='form-control' id="productQuantity" placeholder='Product Quantity' value={productQuantity} onChange={(e) => setProductQuantity(e.target.value)} />
+       </div>
+   
+   </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button type='submit' variant="primary" onClick={addProduct}>
+            Add 
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
-<div className='modal-body'>
-    <form onSubmit={addProduct}>
-        <div className='form-group'>
-            <label>Product Name</label>
-            <input type="text" className='form-control' id="productName" placeholder='Product Name' value={productName} onChange={(e) => setProductName(e.target.value)} />
-        </div>
-        <div className='form-group'>
-            <label>Product Price</label>
-            <input type="text" className='form-control' id="productPrice" placeholder='Product Price' value={productPrice} onChange={(e) => setProductPrice(e.target.value)} />
-        </div>
-        <div className='form-group'>
-            <label>Product Quantity</label>
-            <input type="text" className='form-control' id="productQuantity" placeholder='Product Quantity' value={productQuantity} onChange={(e) => setProductQuantity(e.target.value)} />
-        </div>
-        <button type='submit' >Add</button>
-    </form>
-</div>
 
-    </div>
+</div> 
 );
 
 };
--- ClientSide/src/components/layouts/Header.js
+++ ClientSide/src/components/layouts/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   
      <nav class="navbar navbar-expand-lg bg-dark ">
   <div class="container-fluid">
-    <a class="navbar-brand text-light" href="#">Navbar</a>
+    <a class="navbar-brand text-light" href="#">Bills management</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
